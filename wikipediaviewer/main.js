fa3fec8525d9ab58c2c1f56efe9c6447d236dce1
cina <memaralicina@gmail.com>
2017-04-24 19:38:03 -0400

    Got random articles working on wiki viewer and a nav bar. Switching to angular

---
+++ wikipediaviewer/main.js
@@ -0,0 +1,37 @@
+//$(function()
+//{
+//
+//    
+//    $("#random_article").on("click", function()
+//    {
+//    
+//    
+//    });
+//
+//
+//
+//
+//
+//
+//
+//});
+
+
+
+angular.module('wikiviewer', []);
+
+angular.module('wikiviewer')
+    .controller("maincontroller",['$http',  function($http)
+{
+
+    
+    this.search = function ()
+    {
+    
+    
+    };
+    
+    
+
+
+}]);
